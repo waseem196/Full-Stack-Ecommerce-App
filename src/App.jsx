fc11e4f9a6bafae396a06c1bea4a29477a708832
unknown <mwassi0316@gmail.com>
2023-11-25 23:13:19 +0500

    Just Started

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="w-full h-screen flex justify-center items-center bg-violet-300 font-bold">
+      React Tailwind Full Stack App
+    </div>
+  );
+};
+
+export default App;
